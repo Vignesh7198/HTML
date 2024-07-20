0b4b0b6037c1b2141aea428f834447152ac2a1a6
Vignesh7198 <83552579+Vignesh7198@users.noreply.github.com>
2024-07-20 09:26:37 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+result = 6%5==0?"true":"false"
+
+console.log(result);
+
+let day="ggg";
+
+switch(day){
+
+    case 'Monday':
+console.log(day);
+}
+
+num = 9
+num2 = 4
+
+console.log(`the addition
+              of ${num} and is ${num2}`)
+
+
+              let i = 1;
+
+// while(i<=5){
+//     console.log("OK");
+// }
+
+for(j=0;j<2;j++){
+console.log("test")
+}
